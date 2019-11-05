9e33e1a10394ae2ae8f93e20ac9de3a162fd54a4
Joseorina <orinangare@gmail.com>
2019-11-06 02:38:31 +0300

    Random Forest Regression

--- RFR/random_forest_regression.py
+++ RFR/random_forest_regression.py
@@ -1,1 +1,28 @@
 #Random Forest Regression
+
+#Importing libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+#Fitting Random Forest REgression to dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators=100, random_state=0)
+regressor.fit(X, y)
+
+#Predicting a new result
+y_pred = regressor.predict([[6.5]])
+
+#Visulaizing the regression results
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape(len(X_grid), 1)
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title('Random Forest Regression')
+plt.xlabel('Position Label')
+plt.ylabel('Salary')
+plt.show()
